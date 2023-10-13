ea13aae537495501373033341ad422a29f7eff39
Naveen Prajapati <prajapatinaveen9719@gmail.com>
2023-10-13 21:00:20 +0530

    string interpolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { OneWayStringInterpolationComponent } from './one-way-string-interpolation/one-way-string-interpolation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    OneWayStringInterpolationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/one-way-string-interpolation/one-way-string-interpolation.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'string-interpolation',
+  templateUrl: './one-way-string-interpolation.component.html',
+  styleUrls: ['./one-way-string-interpolation.component.css']
+})
+
+
+export class OneWayStringInterpolationComponent {
+Name:string ='Mahadev'
+Age:Number=0
+
+gods={
+  Name:"Om Namah Shivaay",
+  Address:"Kailash",
+}
+
+
+
+}
