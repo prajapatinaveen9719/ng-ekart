7cdcea9f0a523fafa9f4e3d80bfdc863096b4ff0
Naveen Prajapati <prajapatinaveen9719@gmail.com>
2023-10-13 21:16:55 +0530

    property binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { OneWayStringInterpolationComponent } from './one-way-string-interpolation/one-way-string-interpolation.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    OneWayStringInterpolationComponent
+    OneWayStringInterpolationComponent,
+    PropertyBindingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/property-binding/property-binding.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'property-binding',
+  templateUrl: './property-binding.component.html',
+  styleUrls: ['./property-binding.component.css']
+})
+export class PropertyBindingComponent {
+  ishidden:boolean=false
+
+  inputValue:string='Value property using property binding of input tag'
+
+  inputValue2:string='Value property using interpolation binding of input tag'
+
+
+}
